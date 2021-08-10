9b17e62ee1ac904cce596a6cac870e57a281d703
Ariful Hoque <hq.ariful@gmail.com>
2021-08-10 21:14:31 +0600

    updated to textarea input field

--- forms.py
+++ forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField
+from wtforms import StringField, TextAreaField
 
 class Register(FlaskForm):
     word = StringField("Word")
-    meaning = StringField("Meaning")
+    meaning = TextAreaField("Meaning",render_kw={"rows": 10, "cols": 20})
 
